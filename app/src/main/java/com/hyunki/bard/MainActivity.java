3ed6bdb5c4ff0a94b83272db3bec074725e1767b
hyunki <dhchung317@gmail.com>
2019-03-29 04:28:18 -0400

    add fire base and mlkit. initial app commit

---
+++ app/src/main/java/com/hyunki/bard/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hyunki.bard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
